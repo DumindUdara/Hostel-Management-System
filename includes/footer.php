b3971abe16ff1f56c29300b7c4fcad22cba9c89b
Dumindu Udara <duminduudara81@gmail.com>
2023-07-21 15:34:30 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    &copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by Dumindu Udara & Team
+</footer>
---
+++ student/counters/booked-count.php
@@ -0,0 +1,8 @@
+<?php
+// Booked count
+    include '../includes/dbconn.php';
+
+    $sql = "SELECT id FROM registration";
+                $query = $mysqli->query($sql);
+                echo "$query->num_rows";
+?>
